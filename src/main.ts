91e20cb5245b68c40feaa5f02eaf14adba3cb213
Alexander Holub <holub.alexander.it@gmail.com>
2023-04-12 23:09:26 +0300

    test: test cookie-parser library

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 // const PORT = process.env.PORT || 5000;
 
@@ -7,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
