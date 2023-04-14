38a0a165354acce0eb7322ae547b473ba1cc6aa7
Alexander Holub <holub.alexander.it@gmail.com>
2023-04-14 22:46:55 +0300

    feat: add basic authorization for endpoints users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,10 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  NotFoundException,
-  Param,
-  Post,
-  Query,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, Post, Query } from '@nestjs/common';
 import { UsersWriteRepository } from './repositories/users.write.repository';
 import { UsersService } from './users.service';
 import { PaginationUsersDto } from './dto/pagination-users.dto';
 import { CreateUserDto } from './dto/create.dto';
+import { BasicAuthGuard } from '../auth/guards/basic-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -23,18 +14,21 @@ export class UsersController {
   ) {}
 
   @Get()
+  @UseGuards(BasicAuthGuard)
   findAll(@Query() queryParams: PaginationUsersDto) {
     return this.usersService.findAll(queryParams);
   }
 
   @Post()
   @HttpCode(201)
+  @UseGuards(BasicAuthGuard)
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body);
   }
 
   @Delete('/:id')
   @HttpCode(204)
+  @UseGuards(BasicAuthGuard)
   async deleteUserHandler(@Param('id') id: string) {
     const res = await this.usersWriteRepository.deleteOne(id);
 
