8cf152f522c68009b98bd4432d5ad151e54ed017
Alexander Holub <holub.alexander.it@gmail.com>
2023-07-30 16:18:30 +0300

    test: test

--- src/application/Blogger/handlers/find-all-blogger-comments.handler.ts
+++ src/application/Blogger/handlers/find-all-blogger-comments.handler.ts
@@ -8,6 +8,11 @@ import { BloggerMapper } from '../mappers/blogger.mapper';
 import { Paginator } from '../../../common/interfaces';
 import { CommentBloggerViewModel } from '../interfaces';
 import { CommentViewModel } from '../../Comments/interfaces';
+import { InjectModel } from '@nestjs/mongoose';
+import { Blog, BlogDocument } from '../../../entity/blog.entity';
+import { Model } from 'mongoose';
+import { Post, PostDocument } from '../../../entity/post.entity';
+import { ObjectId } from 'mongodb';
 
 export class FindAllBloggerCommentsCommand {
   constructor(public paginationSortBlogDto: PaginationBlogDto, public userLogin: string) {}
@@ -19,6 +24,8 @@ export class FindAllBloggerCommentsHandler {
     private readonly usersQueryRepository: UsersQueryRepository,
     private readonly postsQueryRepository: PostsQueryRepository,
     private readonly commentsQueryRepository: CommentsQueryRepository,
+    @InjectModel(Blog.name) private blogCollection: Model<BlogDocument>,
+    @InjectModel(Post.name) private postCollection: Model<PostDocument>,
   ) {}
 
   public async execute(
@@ -29,20 +36,21 @@ export class FindAllBloggerCommentsHandler {
     if (!user) {
       throw new UnauthorizedException();
     }
+    const blogIds: string[] = await this.blogCollection.find({ 'bloggerInfo.login': command.userLogin }, { _id: 1 });
+    const posts = await this.postCollection.find({ 'blog.id': { $in: blogIds } });
+    const postsIds = posts.map((p) => p._id);
+    // const foundPosts = await this.postsQueryRepository.findAllByUserId(user._id);
+    // const postsIds = foundPosts.map((post) => post._id);
 
-    const foundPosts = await this.postsQueryRepository.findAllByUserId(user._id);
-    const postsIds = foundPosts.map((post) => post._id);
     const { meta, items } = await this.commentsQueryRepository.findAllByPostsIds(
       command.paginationSortBlogDto,
       postsIds,
       user._id,
     );
 
-    console.log(foundPosts);
-
     return {
       ...meta,
-      items: BloggerMapper.mapCommentBloggerViewModel(items, foundPosts),
+      items: BloggerMapper.mapCommentBloggerViewModel(items, posts),
     };
   }
 }
